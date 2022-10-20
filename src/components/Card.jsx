6064e855c91e3e49b7dbfcc4bb1f7f7d03cb84ca
B-Oz <bettysoftwareng@gmail.com>
2022-10-20 15:40:54 +0300

    getApi

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 import "./card.css";
 
+// import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+
 const Card = () => {
+  const [data, setData] = useState(null);
+  const getApi = () => {
+    axios("https://randomuser.me/api/")
+      .then((res) => {
+        console.log(res);
+        setData(res.data.result[0]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  getApi();
+
   return (
     <div className=" app  d-flex justify-content-center align-items-center flex-column ">
       <div className="card ">
         <div className="up d-flex flex-row justify-content-center ">
-          <img className="m-3" src="./salı.jpg" alt="user-img" />
+          <img className="m-3 photo" src="./salı.jpg" alt="user-img" />
           <p className="m-3">Beyza Özbilgin</p>
         </div>
         <div className="card-body">
           <ul className="list-group list-group-flush ">
-            <li className="list-group-item bg-transparent">An item</li>
-            <li className="list-group-item bg-transparent">A second item</li>
-            <li className="list-group-item bg-transparent">A third item</li>
+            <li className="list-group-item bg-transparent"> mail</li>
+            <li className="list-group-item bg-transparent">telefon</li>
+            <li className="list-group-item bg-transparent">adres</li>
           </ul>
         </div>
         <div className="card-footer">
