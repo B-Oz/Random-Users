f00a19e4c91e68d8212914a886cce558941fb761
B-Oz <bettysoftwareng@gmail.com>
2022-10-20 15:05:40 +0300

    card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./card.css";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className=" app  d-flex justify-content-center align-items-center flex-column ">
+      <div className="card ">
+        <div className="up d-flex flex-row justify-content-center ">
+          <img className="m-3" src="./salı.jpg" alt="user-img" />
+          <p className="m-3">Beyza Özbilgin</p>
+        </div>
+        <div className="card-body">
+          <ul className="list-group list-group-flush ">
+            <li className="list-group-item bg-transparent">An item</li>
+            <li className="list-group-item bg-transparent">A second item</li>
+            <li className="list-group-item bg-transparent">A third item</li>
+          </ul>
+        </div>
+        <div className="card-footer">
+          <p>Age : 26</p>
+          <p> Register Date: 2004-07-31</p>
+        </div>
+      </div>
+
+      <button className="btn btn-primary m-3">Random User</button>
+    </div>
+  );
 };
 
 export default Card;
